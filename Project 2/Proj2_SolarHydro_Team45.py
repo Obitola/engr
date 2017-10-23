55ffa15861ec4f932273520f5717a1941b98333b
obitola <oluwatobi.ola@gmail.com>
2017-10-23 14:44:04 -0400

    slight changes

---
+++ Project 2/Proj2_SolarHydro_Team45.py
@@ -0,0 +1,24 @@
+#given
+energy_out = 120
+gravity = 9.81
+water_density = 1000
+
+#inputs
+pump_efficiency
+turbine_efficiency
+pipe_diameter
+pipe_length
+pipe_friction_factor
+reservoir_depth
+bottom_reservoir_elevation
+flow_rate_pump
+flow_rate_turbine
+bend_coefficient_1
+bend_coefficient_2
+
+#outputs
+reservoir_surface_area
+energy_in
+system_efficiency
+fill_time
+empty_time
