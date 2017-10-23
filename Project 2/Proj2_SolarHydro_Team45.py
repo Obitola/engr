6e59d228cec883931b205aa184f2fc26147cb866
obitola <oluwatobi.ola@gmail.com>
2017-10-23 15:20:04 -0400

    yeet

--- Project 2/Proj2_SolarHydro_Team45.py
+++ Project 2/Proj2_SolarHydro_Team45.py
@@ -6,28 +6,26 @@ gravity = 9.81
 water_density = 1000
 
 # #inputs
-# pump_efficiency
-# turbine_efficiency
-# pipe_diameter
-# pipe_length
-# pipe_friction_factor
-# reservoir_depth
-# bottom_reservoir_elevation
-# flow_rate_pump
-# flow_rate_turbine
-# bend_coefficient_1
-# bend_coefficient_2
-#
+# pump_efficiency = input("Enter your pump efficiency:")
+# turbine_efficiency = input("Enter your turbine efficiency:")
+# pipe_diameter = input("Enter your pipe diameter:")
+# pipe_length = input("Enter your pipe length:")
+# pipe_friction_factor = input("Enter your pipe's friction coefficient:")
+# reservoir_depth = input("Enter the reservoir's depth:")
+# bottom_reservoir_elevation = input("Enter the bottom reservoir's elevation:")
+# flow_rate_pump = input("Enter the flow rate of the pump:")
+# flow_rate_turbine = input("Enter the flow rate of the turbine:")
+# bend_coefficient_1 = input("Enter bent coefficient #1:")
+# bend_coefficient_2 = input("Enter bend coefficient #2:")
+
+#intermediate variables
+
 # #outputs
 # reservoir_surface_area
 # energy_in
 # system_efficiency
 # fill_time
 # empty_time
-
-#other variables
-
-
 
 #functions needed
 def energyTurbine(energy_out,turbine_efficiency):
@@ -41,3 +39,5 @@ def energyLossHitting(water_mass, velocity_down):
 
 def massWater(water_density, flow_rate_pump, fill_time, pipe_length, pipe_diameter):
     return water_density * pi * pow()
+
+
