c902c3132d77b77011051e01b6320aba4d7ccd9a
thoma695 <33038235+thoma695@users.noreply.github.com>
2017-10-24 17:27:17 -0400

    Update Proj2_SolarHydro_Team45.py

--- Project 2/Proj2_SolarHydro_Team45.py
+++ Project 2/Proj2_SolarHydro_Team45.py
@@ -12,6 +12,8 @@ pipe_friction_factor
 reservoir_depth
 bottom_reservoir_elevation
 flow_rate_pump
+fluid_velocity_up
+fluid_velocity_down
 flow_rate_turbine
 bend_coefficient_1
 bend_coefficient_2
@@ -25,14 +27,29 @@ empty_time
 
 #other variables
 
-import math
+
 
 #functions needed
 def energyTurbine(energy_out,turbine_efficiency):
     return energy_out * (1 / turbine_efficiency - 1)
 
-def pipeFrictionDown(water_mass, pipe_friction_factor, pipe_length, velocity_down, pipe_diameter):
-    return water_mass * (pipe_friction_factor * pipe_length * velocity_down ** 2)
+def massWater(water_density, flow_rate_pump, fill_time, pipe_length, pipe_diameter): 
+    return water_density * pi * pow((pipe_diameter/2), 2)* pipe_length 
 
-def massWater(water_density, flow_rate_pump, fill_time, pipe_length, pipe_diameter):
-    return water_density*math.pi * math.pow
+def energyPipeFrictionUp(pipe_friction_factor, massWater, pipe_length, fluid_velocity_up, pipe_diameter): 
+    return (massWater*pipe_friction_factor*pipe_length*pow(fluid_velocity_up,2))/(2*pipe_diameter) 
+ 
+def energyPipeFrictionDown(pipe_friction_factor, massWater, pipe_lenth, fluid_velocity_up, pipe_diameter): 
+    return(massWater*pipe_friction_factor* pipe_length*pow(fluid_velocity_down,2))/(2*pipe_diameter) 
+
+def energyPumpLoss(pump_efficiency, energy_in): 
+    return (1-pump_efficiency)/energy_in 
+
+def energyTurbineLoss(energy_out, turbine_efficiency):     
+    return energy_out*(1/(turbine_efficiency-1)) 
+
+def volumetricFlowUp(pipe_diameter, fluid_velocity_up) 
+    return pow((pipe_diameter/2),2)*pi*fluid_velocity_up
+
+def volumetricFlowDoown(pipe_diameter, fluid_velocity_down)
+    return pow((pipe_diameter/2),2)*pi*fluid_velocity_down
